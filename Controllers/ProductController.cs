1100045b1d591c05df9a5c2c5fb2ff8064eb99cd
AysenurBALKAN <balkan.aysenur@gmail.com>
2023-09-26 21:19:16 +0300

    DropdownList

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -16,6 +16,27 @@ namespace MVCStok.Controllers
             var product = db.tbl_urunler.ToList();
             return View(product);
         }
-        
+        [HttpGet]
+        public ActionResult Product_new()
+        {
+            List<SelectListItem> degerler = (from i in db.tbl_kategoriler.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KatwgoriAd,
+                                                 Value = i.KategoriID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Product_new(tbl_urunler p1)
+        {
+            db.tbl_urunler.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
     }
 }
