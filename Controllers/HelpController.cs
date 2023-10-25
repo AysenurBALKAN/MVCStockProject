a9e9f0fb66c6bf27737a5337f6294e4ea72bb8a3
AysenurBALKAN <balkan.aysenur@gmail.com>
2023-10-25 19:47:01 +0300

    revised1

---
+++ Controllers/HelpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStok.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult help()
+        {
+            return View();
+        }
+    }
+}
